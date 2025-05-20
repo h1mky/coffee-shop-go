598d1d1e0df8167ed22f9be7a9116a38faafa63e
zvezd <adelevnikita@gmail.com>
2025-05-20 15:00:27 +0300

    do more readable code

--- iternal/http/postNewCardHandler.go
+++ iternal/http/postNewCardHandler.go
@@ -14,19 +14,16 @@ func PostNewCardHandler(w http.ResponseWriter, r *http.Request) {
 
 	var card models.CoffeeCardInput
 
-	err := jsonParse(w, r, &card)
-	if err != nil {
-		http.Error(w, "failed parse json", http.StatusInternalServerError)
+	if err := jsonParse(w, r, &card); err != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
 		return
 	}
-	err = validate.ValidateStruct(card)
-	if err != nil {
+	if err := validate.ValidateStruct(card); err != nil {
 		http.Error(w, "error on validate json", http.StatusBadRequest)
 		return
 	}
 
-	err = db.PostNewCard(ctx, card)
-	if err != nil {
+	if err := db.PostNewCard(ctx, card); err != nil {
 		http.Error(w, "failed to save data", http.StatusBadRequest)
 		return
 	}
