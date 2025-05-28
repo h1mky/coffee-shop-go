a5b88edacbdb179f559a7fd58e8dffc3ea7a943b
zvёzd <adelevnikita@gmail.com>
2025-05-28 16:25:10 +0300

    add os.getnv connect

--- db/dbConnect.go
+++ db/dbConnect.go
@@ -2,27 +2,17 @@ package db
 
 import (
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
 var DB *sqlx.DB
 
 func Connect() {
-	password := os.Getenv("DB_PASSWORD")
-	user := os.Getenv("DB_USER")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	dbname := os.Getenv("DB_NAME")
-
-	if password == "" {
-		log.Fatal("DB_PASSWORD env variable is not set")
-	}
-
-	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=require", user, password, host, port, dbname)
+	connect := os.Getenv("DB_CONNECT")
+	dsn := fmt.Sprintf("%s", connect)
 
 	var err error
 	DB, err = sqlx.Connect("postgres", dsn)
