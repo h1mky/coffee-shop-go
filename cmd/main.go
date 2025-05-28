f71150ec4c410aa7dbc2d616e2106d23db578e06
zvёzd <adelevnikita@gmail.com>
2025-05-28 17:43:49 +0300

    add cors new settings

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"coffee-shop/db"
 	"coffee-shop/internal/router"
+	"fmt"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("No .env file found, continuing...")
+	}
 
 	db.Connect()
 
--- db/dbConnect.go
+++ db/dbConnect.go
@@ -1,43 +1,25 @@
 package db
 
 import (
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
-	"strings"
 )
 
 var DB *sqlx.DB
 
 func Connect() {
 	connect := os.Getenv("DB_CONNECT")
-	if connect == "" {
-		log.Fatal("DB_CONNECT environment variable is not set")
-	}
-
-	// Добавляем SSL параметры если их нет
-	if !strings.Contains(connect, "sslmode") {
-		if strings.Contains(connect, "?") {
-			connect += "&sslmode=require"
-		} else {
-			connect += "?sslmode=require"
-		}
-	}
-
-	log.Printf("Attempting to connect to database...")
+	dsn := fmt.Sprintf("%s", connect)
 
 	var err error
-	DB, err = sqlx.Connect("postgres", connect)
+	
+	log.Println("Trying to connect with DSN:", dsn)
+
+	DB, err = sqlx.Connect("postgres", dsn)
 	if err != nil {
-		log.Printf("Connection string: %s", connect)
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
-
-	// Проверяем соединение
-	if err := DB.Ping(); err != nil {
-		log.Fatalf("Unable to ping database: %v\n", err)
-	}
-
-	log.Println("Successfully connected to database")
 }
